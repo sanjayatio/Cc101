34a4e684a99fac0297afc161ec6a01000c5f69af
s.tio <sanjaya.tio@gmail.com>
2026-06-21 17:01:53 +0700

    hack: comitting for another project

--- gfl2_web/data/data_owner.js
+++ gfl2_web/data/data_owner.js
@@ -24,3 +24,13 @@ const DATA_TASK_TIMED = [
   ["Expansion Drill", "2026-06-02", {1:40, 2:58, 3:58}],
   ["Extreme Peak",    "2026-06-09", {1:0,  2:58, 3:98}],
 ];
+
+/**
+ * Resources per owner.
+ * @type {[resource: string, counts: { [ownerId: number]: number }][]}
+ */
+const DATA_RESOURCES = [
+  ["Tickets",         {1: 160, 2:  50, 3: 270}],
+  ["Collapse Pieces", {1: 601, 2:1046, 3: 319}],
+  ["Dorittos",        {1:1877, 2: 694, 3:4815}],
+];
